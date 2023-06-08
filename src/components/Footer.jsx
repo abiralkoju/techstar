3a944fd1fcdb053a1cc6bcf800ed131e3869fcec
Abiral koju <abiral.koju@gmail.com>
2023-06-08 22:14:05 +0545

    day-2 files added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechStar</h1>
+        <p>@all rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://github.com/abiralkoju" target="blank">
+            Github
+          </a>
+          <a href="https://instagram.com" target="blank">
+            Instagram
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillInstagram,
+  AiFillFacebook,
+} from "react-icons/ai";
 
 const Home = () => {
   return (
     <>
-      <div className="home">
+      <div className="home" id="home">
         <main>
           <h1>TechStar</h1>
           <p>Provides tech support to you.</p>
@@ -19,6 +24,43 @@ const Home = () => {
             We are one of the best in business. Hardware and software every
             problem are to be solved by us.
           </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis
+            velit minus non, qui commodi accusantium quam voluptatum cum sit
+            eligendi vitae aut illum expedita enim sed quia autem cupiditate
+            ad?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem cumque autem atque suscipit provident veritatis quod
+            eligendi, architecto nobis nemo.
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div>
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div>
+              <AiFillInstagram />
+              <p>Google</p>
+            </div>
+
+            <div>
+              <AiFillFacebook />
+              <p>Google</p>
+            </div>
+          </article>
         </div>
       </div>
     </>
